09acfae38ec07e1412b5cdf838850e89e8e0af1c
James Fisher <jfisher396@hotmail.com>
2020-08-14 20:05:15 -0700

    created projector component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,19 +5,23 @@ class NavBar extends Component {
   render() {
     return (
       <div className="top-bar">
-  <div className="top-bar-left">
-    <ul className="dropdown menu" data-dropdown-menu>
-      <li className="menu-text">Site Title</li>
-      <li><a href="#">One</a></li>
+        <div className="top-bar-left">
+          <ul className="dropdown menu" data-dropdown-menu>
+            <li className="menu-text">
+              <Link to="/" className="link">
+                AV Equipment Rental
+              </Link>
+            </li>
+            {/* <li><a href="#">One</a></li>
       <li><a href="#">Two</a></li>
-      <li><a href="#">Three</a></li>
-    </ul>
-  </div>
-  <div className="top-bar-right">
-    
-  </div>
-</div>
-    )
+      <li><a href="#">Three</a></li> */}
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          800-555-1234
+        </div>
+      </div>
+    );
   }
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                <h1>This is the home page</h1>
+          <div className="grid-x grid-margin-x">
+            <div className="cell medium-6 large-4">
+              <div className="callout warning">
+                <h5>This is a callout.</h5>
+                <p>
+                  It has an easy to override visual style, and is appropriately
+                  subdued.
+                </p>
+                <Link to="/inventory">
+                  EQUIPMENT
+                </Link>
+              </div>
             </div>
-        )
+          </div>
+        );
     }
 }
