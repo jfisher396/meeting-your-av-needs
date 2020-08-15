e18a7d2b75f855cb7dc30a4715745039ab6593de
James Fisher <jfisher396@hotmail.com>
2020-08-14 21:07:58 -0700

    equipment tables designed

--- client/src/components/ProjectorsList.js
+++ client/src/components/ProjectorsList.js
@@ -14,28 +14,28 @@ export default class Projectors extends Component {
   render() {
     const { projectors } = this.state;
     return (
-      <>
-        <h1>This is the inventory page</h1>
-
-        {projectors.map((item) => (
-          <div className="container">
+      <> 
+        <div className="container">
+            <h3>Projectors</h3>
             <table className="table">
-              <thead>
-                <tr>
-                  <th scope="col">Model</th>
-                  <th scope="col">Quantity</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr key={item._id}>
-                  
-                  <td>{item.model}</td>
-                  <td>{item.quantity}</td>
-                </tr>
-              </tbody>
+                <thead>
+                    <tr>
+                    <th scope="col">Model</th>
+                    <th scope="col">Quantity Available</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {projectors.map((item) => {
+                     return   (   
+                        <tr key={item._id}>
+                            <td>{item.model}</td>
+                            <td>{item.quantity}</td>
+                        </tr>
+                     )
+                     })}
+                </tbody>
             </table>
-          </div>
-        ))}
+        </div>
       </>
     );
   }
--- client/src/pages/Inventory.js
+++ client/src/pages/Inventory.js
@@ -7,7 +7,35 @@ export default class Inventory extends Component {
     render() {
         
         return (
-            <Projectors />
-        )
+          <>
+            <div className="grid-container">
+              <div class="grid-x grid-margin-x small-up-2 medium-up-3">
+                <div class="cell">
+                  <div className="card">
+                    <div className="card-section">
+                      <Projectors />
+                    </div>
+                  </div>
+                </div>
+
+                <div class="cell">
+                  <div className="card">
+                    <div className="card-section">
+                      <Projectors />
+                    </div>
+                  </div>
+                </div>
+
+                <div class="cell">
+                  <div className="card">
+                    <div className="card-section">
+                      <Projectors />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        );
     }
 }
