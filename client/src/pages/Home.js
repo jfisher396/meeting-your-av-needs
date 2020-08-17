8b532d4d4c6c2b36f03a4c720d74269a8259e0d3
James Fisher <jfisher396@hotmail.com>
2020-08-17 13:56:32 -0700

    created welcome box

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 
 export default class Home extends Component {
     render() {
         return (
           <div className="grid-x grid-margin-x">
-            <div className="cell medium-6 large-4">
-              <div className="callout warning">
-                <h5>This is a callout.</h5>
-                <p>
-                  It has an easy to override visual style, and is appropriately
-                  subdued.
-                </p>
-                <Link to="/inventory">
-                  EQUIPMENT
-                </Link>
+            <div className="homeBox cell medium-6 large-4">
+              <div className="media-object">
+                <div className="media-object-section">
+                  <div className="thumbnail">
+                    <img
+                      src="http://www.fillmurray.com/200/300"
+                      alt="fill murray"
+                    />
+                  </div>
+                </div>
+                <div className="media-object-section">
+                  <h4>Dreams feel real while we're in them.</h4>
+                  <p>
+                    I'm going to improvise. Listen, there's something you should
+                    know about me... about inception. An idea is like a virus,
+                    resilient, highly contagious. The smallest seed of an idea
+                    can grow. It can grow to define or destroy you.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require('path')
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -13,13 +12,9 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-// app.get("/test", (req,res) =>{
-//     res.send("This is only a test.")
-// });
+
 app.use(routes);
 // app.get("*", (req,res) => {
-//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/avrentals");
