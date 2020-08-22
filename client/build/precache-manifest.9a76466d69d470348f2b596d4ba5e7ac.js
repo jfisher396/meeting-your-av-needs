f07af605ab09720d7d751af66f14949d80188ca3
James Fisher <jfisher396@hotmail.com>
2020-08-22 16:29:55 -0700

    build

---
+++ client/build/precache-manifest.9a76466d69d470348f2b596d4ba5e7ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd531a05c8d1422c0094a468c0f69f3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "200513d4c7b61b470b5b",
+    "url": "/static/css/main.2f5129a8.chunk.css"
+  },
+  {
+    "revision": "38ea539e6c0333fc7944",
+    "url": "/static/js/2.a926d368.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a926d368.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "200513d4c7b61b470b5b",
+    "url": "/static/js/main.fada0255.chunk.js"
+  },
+  {
+    "revision": "4353c8dabc71c9dc4372",
+    "url": "/static/js/runtime-main.14d84dd7.js"
+  },
+  {
+    "revision": "7cf9ac6d7636cd5ff9881d8f0863f431",
+    "url": "/static/media/meeting room.7cf9ac6d.jpg"
+  }
+]);
