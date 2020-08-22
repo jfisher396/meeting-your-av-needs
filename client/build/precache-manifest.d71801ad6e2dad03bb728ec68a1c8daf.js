8b57b9147b29665f1aad6b0543ad85b192479324
Maimell <malinka77@gmail.com>
2020-08-22 16:12:09 -0700

    make it better

---
+++ client/build/precache-manifest.d71801ad6e2dad03bb728ec68a1c8daf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777f191b3d33ef1080f22e55a8d545e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d66da98f5247ec8867d0",
+    "url": "/static/css/main.2f5129a8.chunk.css"
+  },
+  {
+    "revision": "d37866c04fc0ac3d178d",
+    "url": "/static/js/2.c0bdbcf8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c0bdbcf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d66da98f5247ec8867d0",
+    "url": "/static/js/main.8fff78c4.chunk.js"
+  },
+  {
+    "revision": "4353c8dabc71c9dc4372",
+    "url": "/static/js/runtime-main.14d84dd7.js"
+  },
+  {
+    "revision": "7cf9ac6d7636cd5ff9881d8f0863f431",
+    "url": "/static/media/meeting room.7cf9ac6d.jpg"
+  }
+]);
