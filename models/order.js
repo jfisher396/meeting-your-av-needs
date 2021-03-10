382e6f4a1200e63b2cc98c0e647670502dfe9504
James Fisher <jfisher396@hotmail.com>
2021-03-10 11:30:35 -0800

    added timestamp to orders model

--- models/order.js
+++ models/order.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
-    items: []
+    items: [],
+
+    orderCreated: {
+        type: Date,
+        default: Date.now
+    }
     
 });
 
